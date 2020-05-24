d71b06efbc652a56b6b0d1d54556721e965ba48e
Nicola Molinari <nicola.molinari@commercetools.de>
2020-05-24 19:59:00 +0200

    fix: refresh SW

---
+++ gatsby-browser.js
@@ -0,0 +1,3 @@
+// https://github.com/gatsbyjs/gatsby/pull/9087
+// https://www.gatsbyjs.org/docs/browser-apis/#onServiceWorkerUpdateReady
+exports.onServiceWorkerUpdateReady = () => window.location.reload(true);
