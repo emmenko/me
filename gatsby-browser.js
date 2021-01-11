791419bbd2c4f3bc308d50fd84128d55ff465dc4
Nicola Molinari <nicola.molinari@commercetools.de>
2021-01-11 17:21:55 +0100

    refactor: improve styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,3 @@
 // https://github.com/gatsbyjs/gatsby/pull/9087
 // https://www.gatsbyjs.org/docs/browser-apis/#onServiceWorkerUpdateReady
-exports.onServiceWorkerUpdateReady = () => window.location.reload(true);
+exports.onServiceWorkerUpdateReady = () => window.location.reload();
